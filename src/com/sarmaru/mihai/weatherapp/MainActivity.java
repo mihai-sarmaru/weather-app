fea3a3873eb275d437db01bd12249cd941bba560
Mihai Sarmaru <predator7w@gmail.com>
2014-09-13 12:08:38 +0300

    Initialized ActionBar to use tabs and new adapter

--- src/com/sarmaru/mihai/weatherapp/MainActivity.java
+++ src/com/sarmaru/mihai/weatherapp/MainActivity.java
@@ -1,16 +1,44 @@
 package com.sarmaru.mihai.weatherapp;
 
-import android.app.Activity;
+import com.sarmaru.mihai.weatherapp.adapter.TabsPagerAdapter;
+
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
+	
+	private ActionBar actionBar;
+	private TabsPagerAdapter mAdapter;
+	private ViewPager viewPager;
+	
+	// Tab titles
+	private String[] tabs = {"Today", "Tomorrow"}; // TODO: move to strings.xml
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// Initialization
+		viewPager = (ViewPager) findViewById(R.id.pager);
+		actionBar = getActionBar();
+		mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
+		
+		// Set adapter and ActionBar navigation modes
+		viewPager.setAdapter(mAdapter);
+		actionBar.setHomeButtonEnabled(false);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		
+		// Add tabs to ActionBar
+		for (String tabName : tabs) {
+			actionBar.addTab(actionBar.newTab().setText(tabName).setTabListener(this));
+		}
 	}
 
 	@Override
@@ -30,5 +58,23 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
 	}
 }

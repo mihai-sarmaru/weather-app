2ee52af04ca013a19681a3a6cb7df23a4516b5e7
Mihai Sarmaru <predator7w@gmail.com>
2014-09-13 12:06:24 +0300

    Added Today and Tomorrow fragment classes and views

---
+++ src/com/sarmaru/mihai/weatherapp/TodayFragment.java
@@ -0,0 +1,19 @@
+package com.sarmaru.mihai.weatherapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TodayFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater,
+			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		// Inflate fragment XML
+		View rootView = inflater.inflate(R.layout.fragment_today, container, false);
+		return rootView;
+	}
+}

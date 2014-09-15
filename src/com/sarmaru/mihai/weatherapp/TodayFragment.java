7dbd84486e5066eb1664e4be047ef7d4756a4b8f
Mihai Sarmaru <predator7w@gmail.com>
2014-09-15 18:54:05 +0300

    Set typeFace for weather font on icon text views

--- src/com/sarmaru/mihai/weatherapp/TodayFragment.java
+++ src/com/sarmaru/mihai/weatherapp/TodayFragment.java
@@ -1,11 +1,13 @@
 package com.sarmaru.mihai.weatherapp;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class TodayFragment extends Fragment {
 
@@ -14,6 +16,13 @@ public class TodayFragment extends Fragment {
 			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 		// Inflate fragment XML
 		View rootView = inflater.inflate(R.layout.fragment_today, container, false);
+		
+		// Set weatherFont to icon TextView
+		TextView icon = (TextView) rootView.findViewById(R.id.today_icon);
+		Typeface weatherFont = Typeface.createFromAsset(getActivity().getAssets(), "weathericons-font.ttf");
+		icon.setTypeface(weatherFont);
+		
+		// Return view
 		return rootView;
 	}
 }

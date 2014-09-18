7b5012450dd78c9a543a66ac205b9f14ffd62870
Mihai Sarmaru <predator7w@gmail.com>
2014-09-18 19:11:06 +0300

    Added Internet permission + cleanup

--- src/com/sarmaru/mihai/weatherapp/adapter/HttpHandler.java
+++ src/com/sarmaru/mihai/weatherapp/adapter/HttpHandler.java
@@ -3,7 +3,6 @@ package com.sarmaru.mihai.weatherapp.adapter;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 

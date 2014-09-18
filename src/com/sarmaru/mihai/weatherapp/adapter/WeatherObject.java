e8af5a1add8c2fea3f9a9e1bf785ef80c06ebbfa
Mihai Sarmaru <predator7w@gmail.com>
2014-09-18 21:19:58 +0300

    Updated WeatherObject to use generated ids

--- src/com/sarmaru/mihai/weatherapp/adapter/WeatherObject.java
+++ src/com/sarmaru/mihai/weatherapp/adapter/WeatherObject.java
@@ -2,6 +2,8 @@ package com.sarmaru.mihai.weatherapp.adapter;
 
 import java.text.DecimalFormat;
 
+import com.sarmaru.mihai.weatherapp.R;
+
 public class WeatherObject {
 	
 	// Private variables
@@ -25,14 +27,14 @@ public class WeatherObject {
 	public static final int IMPERIAL = 2;
 	
 	// Weather Icons
-	public static final int WEATHER_SUNNY = 1;
-	public static final int WEATHER_CLEAR_NIGHT = 2;
-	public static final int WEATHER_FOGGY = 3;
-	public static final int WEATHER_CLOUDY = 4;
-	public static final int WEATHER_RAINY = 5;
-	public static final int WEATHER_SNOWY = 6;
-	public static final int WEATHER_THUNDER = 7;
-	public static final int WEATHER_DRIZZLE = 8;
+	public static final int WEATHER_SUNNY = R.string.weather_sunny;
+	public static final int WEATHER_CLEAR_NIGHT = R.string.weather_clear_night;
+	public static final int WEATHER_FOGGY = R.string.weather_foggy;
+	public static final int WEATHER_CLOUDY = R.string.weather_cloudy;
+	public static final int WEATHER_RAINY = R.string.weather_rainy;
+	public static final int WEATHER_SNOWY = R.string.weather_snowy;
+	public static final int WEATHER_THUNDER = R.string.weather_thunder;
+	public static final int WEATHER_DRIZZLE = R.string.weather_drizzle;
 	
 	// Empty constructor
 	public WeatherObject() {}

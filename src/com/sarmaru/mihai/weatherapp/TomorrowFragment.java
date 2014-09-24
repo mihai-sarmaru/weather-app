fd1ce2f45426e8d1d0c12d4054f3ff693c6e675e
Mihai Sarmaru <predator7w@gmail.com>
2014-09-24 21:39:18 +0300

    Initialized all views for tomorrow layout

--- src/com/sarmaru/mihai/weatherapp/TomorrowFragment.java
+++ src/com/sarmaru/mihai/weatherapp/TomorrowFragment.java
@@ -11,19 +11,48 @@ import android.widget.TextView;
 
 public class TomorrowFragment extends Fragment {
 	
+	// Tomorrow Views
+	private static TextView tomorrowName, tomorrowIcon, tomorrowTemperature, tomorrowDescription;
+	private static TextView tomorrowForecastOne, tomorrowForecastTwo, tomorrowForecastThree, tomorrowForecastFour;
+	private static TextView tomorrowForecastOneIcon, tomorrowForecastTwoIcon, tomorrowForecastThreeIcon, tomorrowForecastFourIcon;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater,
 			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 		// Inflate fragment XML
 		View rootView = inflater.inflate(R.layout.fragment_tomorrow, container, false);
 		
-		// Set weatherFont to icon TextView
-		TextView icon = (TextView) rootView.findViewById(R.id.tomorrow_icon);
-		Typeface weatherFont = Typeface.createFromAsset(getActivity().getAssets(), "weathericons-font.ttf");
-		icon.setTypeface(weatherFont);
+		// Initialize text views
+		initializeViews(rootView);
 		
 		// Return view
 		return rootView;
 	}
 
+	// Initialize text views
+	private void initializeViews(View rootView) {
+		tomorrowName = (TextView) rootView.findViewById(R.id.tomorrow_name);
+		
+		// Set weatherFont to icon TextView
+		tomorrowIcon = (TextView) rootView.findViewById(R.id.tomorrow_icon);
+		tomorrowForecastOneIcon = (TextView) rootView.findViewById(R.id.tomorrow_forecast1_icon);
+		tomorrowForecastTwoIcon = (TextView) rootView.findViewById(R.id.tomorrow_forecast2_icon);
+		tomorrowForecastThreeIcon = (TextView) rootView.findViewById(R.id.tomorrow_forecast3_icon);
+		tomorrowForecastFourIcon = (TextView) rootView.findViewById(R.id.tomorrow_forecast4_icon);
+		
+		Typeface weatherFont = Typeface.createFromAsset(getActivity().getAssets(), "weathericons-font.ttf");
+		tomorrowIcon.setTypeface(weatherFont);
+		tomorrowForecastOneIcon.setTypeface(weatherFont);
+		tomorrowForecastTwoIcon.setTypeface(weatherFont);
+		tomorrowForecastThreeIcon.setTypeface(weatherFont);
+		tomorrowForecastFourIcon.setTypeface(weatherFont);
+		
+		tomorrowTemperature = (TextView) rootView.findViewById(R.id.tomorrow_temperature);
+		tomorrowDescription = (TextView) rootView.findViewById(R.id.tomorrow_description);
+		
+		tomorrowForecastOne = (TextView) rootView.findViewById(R.id.tomorrow_forecast1);
+		tomorrowForecastTwo = (TextView) rootView.findViewById(R.id.tomorrow_forecast2);
+		tomorrowForecastThree = (TextView) rootView.findViewById(R.id.tomorrow_forecast3);
+		tomorrowForecastFour = (TextView) rootView.findViewById(R.id.tomorrow_forecast4);
+	}
 }
